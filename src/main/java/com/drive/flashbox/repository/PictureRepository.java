0b0b11a0d0da01458055cf0f0b81c401dcd7ae2d
renesuno <renesuno@gmail.com>
2025-02-21 10:16:23 +0900

    add feature

---
+++ src/main/java/com/drive/flashbox/repository/PictureRepository.java
@@ -0,0 +1,10 @@
+package com.drive.flashbox.repository;
+
+import com.drive.flashbox.entity.Picture;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+    // bid와 pid로 Picture를 찾는 메서드
+    Optional<Picture> findByPidAndBoxBid(Long pid, Long bid);
+}
